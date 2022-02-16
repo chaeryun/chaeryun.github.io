7ea5e0d4bd2865f870941cc3f6088b026913f5bc
chaeryun <kimchaeryun.gmail.com>
2022-02-16 23:30:33 +0900

    add 0216

--- Algorithm/6.cpp
+++ Algorithm/6.cpp
@@ -7,4 +7,14 @@ int main() {
 
   int sum1 = a + b;
   int min1 = a - b;
+  int mul1 = a * b;
+  int div1 = a / b;
+  int div2 = a % b;   // 5%3 = 2 나머지 반환
+
+  printf("%d + %d = %d\n", a, b, sum1);
+  printf("%d - %d = %d\n", a, b, min1);
+  printf("%d * %d = %d\n", a, b, mul1);
+  printf("%d / %d = %d\n", a, b, div1);
+  printf("%d + %d = %d\n", a, b, div2);
+  
 }
