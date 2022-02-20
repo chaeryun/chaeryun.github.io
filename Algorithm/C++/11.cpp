9e7bb65abbddc2258978fb9213d4854489a26007
chaeryun <kimchaeryun.gmail.com>
2022-02-21 00:11:00 +0900

    Add 0221

---
+++ Algorithm/C++/11.cpp
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d%d", &a, &b);  //&:∆˜¿Œ≈Õ
+
+    int sum1 = a + b;
+    int min1 = a - b;
+    int mul1 = a * b;
+    int div1 = a / b;
+
+    printf("%d + %d = %d\n", a, b, sum1);
+    printf("%d - %d = %d\n", a, b, min1);
+    printf("%d * %d = %d\n", a, b, mul1);
+    printf("%d / %d = %f\n", a, b, div1); 
+
+}
