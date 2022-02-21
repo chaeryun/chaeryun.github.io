1fbdcde7f9974121ea8a05cd89723a6562aa3e21
chaeryun <48987122+chaeryun@users.noreply.github.com>
2022-02-21 21:35:49 +0900

    Update 11.cpp

--- Algorithm/C++/11.cpp
+++ Algorithm/C++/11.cpp
@@ -1,1 +1,20 @@
 
+# include <stdio.h>
+
+int main() {
+    int a, b;
+
+    scanf("%d%d", &a, &b);  //& : 포인터
+
+    int sum1 = a + b;
+    int min1 = a - b;
+    int mul1 = a * b;
+    int div1 = a / b;
+
+    printf("%d + %d = %d\n", a, b, sum1);
+    printf("%d - %d = %d\n", a, b, min1);
+    printf("%d * %d = %d\n", a, b, mul1);
+    printf("%d / %d = %d\n", a, b, div1); 
+
+
+}
